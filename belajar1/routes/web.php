465b0646dbf47776b25f3b51ef46479148c58d8f
mountain-ux <hasnurtarakan@gmail.com>
2026-04-07 19:54:45 +0800

    fix bug

--- belajar1/routes/web.php
+++ belajar1/routes/web.php
@@ -13,17 +13,12 @@ Route::get('/dashboard', [BarangController::class, 'dashboard'])
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::patch('/profile/photo', [ProfileController::class, 'updatePhoto'])->name('profile.update.photo');
+
     Route::resource('pendaftaran', PendaftaranController::class);
     Route::resource('barang', BarangController::class);
 });
