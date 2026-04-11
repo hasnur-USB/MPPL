c41367ac0f3e542f234f619745515cdc7a2606a9
mountain-ux <hasnurtarakan@gmail.com>
2026-04-11 09:35:35 +0800

    update

---
+++ ecommerce_app/database/migrations/2026_04_11_013303_create_cart_items_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('cart_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('barang_id')->constrained('barangs')->onDelete('cascade');
+            $table->integer('qty')->default(1);
+            $table->timestamps();
+
+            $table->unique(['user_id', 'barang_id']); // agar tidak duplikat barang di cart
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('cart_items');
+    }
+};
